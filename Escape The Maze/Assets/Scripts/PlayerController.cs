27209c5a9d475195459273a0c1d3cde6a8c4ac38
359Kirk <brianabishopaz@gmail.com>
2022-11-17 22:00:00 -0700

    Game v1 finished

---
+++ Escape The Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+
+    private float startTime;
+    private float timeTaken;
+
+    public GameObject playButton;
+    public TextMeshProUGUI curTimeText;
+
+    private bool isPlaying;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!isPlaying)
+            return;
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+        curTimeText.text = (Time.time - startTime).ToString("F2");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("End"))
+        {
+            End();
+        }
+    }
+
+    public void Begin()
+    {
+        startTime = Time.time;
+        isPlaying = true;
+        playButton.SetActive(false);
+    }
+
+    void End()
+    {
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+        playButton.SetActive(true);
+        Leaderboard.instance.SetLeaderboardEntry(-Mathf.RoundToInt(timeTaken * 1000.0f));
+    }
+
+
+}
